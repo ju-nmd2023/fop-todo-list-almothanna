0bef1ee3f3dddf8501fa67a3bc30b74c3c72480b
almothanna <148761376+almothanna@users.noreply.github.com>
2025-05-22 01:38:37 +0200

    added done tasks

--- java.js
+++ java.js
@@ -13,14 +13,18 @@ function renderTasks() {
   tasks.forEach((task, index) => {
     const li = document.createElement("li");
 
-    const span = document.createElement("span");
-    span.textContent = task.text;
-    if (task.done) span.classList.add("done");
-    span.onclick = () => {
-      tasks[index].done = !tasks[index].done;
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = task.done;
+    checkbox.onchange = () => {
+      tasks[index].done = checkbox.checked;
       saveTasks();
       renderTasks();
     };
+
+    const span = document.createElement("span");
+    span.textContent = task.text;
+    if (task.done) span.classList.add("done");
 
     const delBtn = document.createElement("button");
     delBtn.textContent = "Delete";
@@ -31,6 +35,7 @@ function renderTasks() {
       renderTasks();
     };
 
+    li.appendChild(checkbox);
     li.appendChild(span);
     li.appendChild(delBtn);
     taskList.appendChild(li);
